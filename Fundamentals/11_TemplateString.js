69adb5e1b47e3cb42785bc2e2020a29e5845b89c
Wesley Lima <wsouza1992@hotmail.com>
2022-03-22 15:03:36 +0000

    Added 11_TemplateString and 12_Function on Fundamentals

---
+++ Fundamentals/11_TemplateString.js
@@ -0,0 +1,10 @@
+{
+    const prodName = "Iphone X"
+    const prodType = "Mobile"
+    const templateString = 
+        `Product: ${prodName}
+         Type: ${prodType}
+        `
+    console.log(templateString);
+        
+}
---
+++ Fundamentals/12_Function.js
@@ -0,0 +1,4 @@
+{
+    const toUpper = text => text.toUpperCase();
+    console.log(`Hey you... ${toUpper(`be careful`)}!!!`);
+}
